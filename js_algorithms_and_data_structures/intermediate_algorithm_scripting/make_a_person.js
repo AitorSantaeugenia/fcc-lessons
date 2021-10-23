8354444e730927c2a7c0f068e681e4684ff81431
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-10-23 21:33:32 +0200

    lessons JS done

---
+++ js_algorithms_and_data_structures/intermediate_algorithm_scripting/make_a_person.js
@@ -0,0 +1,50 @@
+/*
+Make a Person
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods that take 
+an argument must accept only one argument and it has to be a string. These methods must be the 
+only available means of interacting with the object.
+*/
+var Person = function(firstAndLast) {
+	// Only change code below this line
+	// Complete the method below and implement the others similarly
+	let [ firstName, lastName ] = firstAndLast.split(' ');
+
+	this.getFirstName = function() {
+		return firstName;
+	};
+
+	this.getLastName = function() {
+		return lastName;
+	};
+
+	this.getFullName = function() {
+		return `${firstName} ${lastName}`;
+	};
+
+	this.setFirstName = function(first) {
+		firstName = first;
+		return firstName;
+	};
+
+	this.setLastName = function(last) {
+		lastName = last;
+		return lastName;
+	};
+
+	this.setFullName = function(firstAndLast) {
+		firstName = firstAndLast.split(' ')[0];
+		lastName = firstAndLast.split(' ')[1];
+		return `${firstName} ${lastName}`;
+	};
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
