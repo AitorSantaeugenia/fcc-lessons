f169c3bc8b4c588909ec216368a5b31da7516c07
AitorSantaeugenia <aitorsantaeugenia@gmail.com>
2023-09-01 21:06:21 +0200

    feat: lesson 1 start

---
+++ new-foundation-C#-microsoft-certification/1-write-your-first-code-using-C#/1-FCC_1.cs
@@ -0,0 +1,19 @@
+/* 
+N= NO
+Y= Yes
+
+Question 1:
+----------------------------------------------------------------------------------------------------------------
+This challenge will be partially completed on the Microsoft Learn platform. Follow these instructions to complete 
+the challenge:
+
+Go to https://learn.microsoft.com/training/modules/csharp-write-first/ and complete all the tasks in the 
+"Write Your First C# Code" module. This is required to earn the "Write Your First Code Using C#" trophy on 
+Microsoft Learn, and qualify for the certification exam.
+When you are finished, come back and correctly answer the question below.
+What is the difference between Console.Write and Console.WriteLine?
+
+N - Console.Write prints the output on a new line.
+N- Console.WriteLine prints the output on a new line.
+Y- Console.WriteLine appends a new line after the output.
+*/
---
+++ new-foundation-C#-microsoft-certification/1-write-your-first-code-using-C#/Microsoft_First_Code/1-Ejercicio_Escribir_el_primer_codigo.cs
@@ -0,0 +1,51 @@
+//Ejemplo Console.WriteLine
+Console.WriteLine("Hello World!");
+
+//Ejemplo Console.Write
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+/*
+//Ejemplos de cómo NO hacerlo
+console.WriteLine("Hello World!");
+
+Console.WriteLine('Hello World!');
+
+// Console.WriteLine("Hello World!");
+*/
+
+//Completar el desafio
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+
+// ---------------
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+
+/*
+N= NO
+Y= YES
+Knowledge check
+
+Check your knowledge
+
+1. What is the primary job of the compiler? 
+N-The compiler primarily locates spelling mistakes in your code.
+N-The compiler primarily executes your code.
+Y-The compiler primarily converts your code into an executable format that the computer can understand.
+
+2. Which of the following statements is true about C#? 
+N-C# is case insensitive.
+N-Console is a method, and WriteLine() is a class.
+Y-You use double quotation marks to create a literal string.
+
+3. What is wrong with this line of code? Console.WriteLine("What is wrong with me?") 
+N-The L in WriteLine should be lower-case.
+Y-It's missing a semi-colon at the end
+N-The string should use single-quotes.
+
+*/
