f5caf65e8d05ca5e97e7a23b22f109f81befcf3e
AitorSantaeugenia <aitorsantaeugenia@gmail.com>
2023-09-01 21:24:06 +0200

    feat: ex1 and ex2 done

---
+++ new-foundation-C#-microsoft-certification/1-write-your-first-code-using-C#/2-Store_and_retrieve_data_using_literal_andvariable_values/2-variables.cs
@@ -0,0 +1,91 @@
+Console.WriteLine(123);
+
+//coma flotante
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+
+//sufijo literal m
+Console.WriteLine(12.39816m);
+
+//booleanos
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
+
+// DECLARACION DE VARIABLES
+string firstName ;
+
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;
+
+// Examples
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+//VARIABLES IMPLICITAS
+var message = "Hello world!";
+
+// Exercise
+var name = "Bob";
+var mail = 3;
+var tmp = 34.4;
+
+
+Console.Write("Hello ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(mail);
+Console.Write(" in your inbox. The temperature is ");
+Console.Write(tmp);
+Console.Write(" celsius.");
+
+/*
+Y= YES
+N = NO
+
+Prueba de conocimientos
+
+Comprobación de conocimientos
+
+1. ¿Cuáles de los siguientes nombres de variable se deben evitar? 
+
+Y-$DATA
+N-registrationComplete
+N-flag
+
+2. Cuál es el problema de esta línea de código: var message; 
+
+N-var no es un tipo de datos.
+Y-Uso de la palabra clave var sin inicializar la variable
+N-Una variable denominada message siempre debe ser una cadena.
+
+3. ¿Cuál de las siguientes salidas es la de Console.WriteLine(34.40M);? 
+
+N-34.40M
+N-34.4
+Y-34.40
+
+4. ¿Cuál de las siguientes líneas de código crea una variable correctamente? 
+
+N-int x = 12.3m;
+Y-decimal x = 12.3m;
+N-bool x = 'False';
+
+*/
