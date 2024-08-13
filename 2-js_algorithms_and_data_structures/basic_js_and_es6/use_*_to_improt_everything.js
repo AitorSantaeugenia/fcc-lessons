84b79530add858d3889fd6b9db9212ad18c38537
aitor santaeugenia mari <aitorsantaeugenia@gmail.com>
2024-08-13 22:35:29 +0200

    fix (HEAD & route): fixing the HEAD and route of the repo

--- 2-js_algorithms_and_data_structures/basic_js_and_es6/use_*_to_improt_everything.js
+++
@@ -1,6 +0,0 @@
-// Only change code above this line
-
-stringFunctions.uppercaseString('hello');
-stringFunctions.lowercaseString('WORLD!');
-
-import * as stringFunctions from './string_functions.js';
