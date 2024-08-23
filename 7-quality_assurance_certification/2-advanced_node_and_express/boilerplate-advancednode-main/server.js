2f4984502917d9c803597633846c86665eb1f623
aitor santaeugenia mari <aitorsantaeugenia@gmail.com>
2024-08-24 01:28:09 +0200

    feat(2-advanced_node_and_express): ex6 auth strategies done

--- 7-quality_assurance_certification/2-advanced_node_and_express/boilerplate-advancednode-main/server.js
+++ 7-quality_assurance_certification/2-advanced_node_and_express/boilerplate-advancednode-main/server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require("express-session");
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -48,6 +49,15 @@ myDB(async client => {
       done(null, doc);
     });
   });
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  }));
   // Be sure to add this...
 }).catch(e => {
   app.route('/').get((req, res) => {
